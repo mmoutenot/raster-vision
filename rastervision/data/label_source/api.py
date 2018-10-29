e8edbb44aea3284ad60e83db981574752049f7d4
Lewis Fishgold <lewfish@gmail.com>
2018-10-29 18:13:53 -0400

    Log warnings about using deprecated keys

--- rastervision/data/label_source/api.py
+++ rastervision/data/label_source/api.py
@@ -1,5 +1,8 @@
 # flake8: noqa
 
+from rastervision.task.api import (
+    OBJECT_DETECTION, CHIP_CLASSIFICATION, SEMANTIC_SEGMENTATION)
+
 # Registry Keys
 LABEL_SOURCE = 'LABEL_SOURCE'
 
@@ -8,7 +11,13 @@ OBJECT_DETECTION_GEOJSON = 'OBJECT_DETECTION_GEOJSON'
 CHIP_CLASSIFICATION_GEOJSON = 'CHIP_CLASSIFICATION_GEOJSON'
 SEMANTIC_SEGMENTATION_RASTER = 'SEMANTIC_SEGMENTATION_RASTER'
 
-# OBJECT_DETECTION, CHIP_CLASSIFICATION, SEMANTIC_SEGMENTATION are the keys to use
-# and they are already defined in rastervision.task.api
+label_source_deprecated_map = {
+    OBJECT_DETECTION_GEOJSON: OBJECT_DETECTION,
+    CHIP_CLASSIFICATION_GEOJSON: CHIP_CLASSIFICATION,
+    SEMANTIC_SEGMENTATION_RASTER: SEMANTIC_SEGMENTATION
+}
+
+# OBJECT_DETECTION, CHIP_CLASSIFICATION, SEMANTIC_SEGMENTATION are the current keys to
+# use and they are already defined in rastervision.task.api
 
 from rastervision.data.label_source.label_source_config import LabelSourceConfig
